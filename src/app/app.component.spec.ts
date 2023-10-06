c7cf155249dae05b02cec990855e4e93a277335b
Nelrik <addevel@outlook.com>
2023-10-05 19:01:24 -0600

    Update with my name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-app-jueves'`, () => {
+  it(`should have as title 'angularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app-jueves');
+    expect(app.title).toEqual('angularApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app-jueves app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularApp app is running!');
   });
 });
